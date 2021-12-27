82b8236387654842fe15cd0c870ef29de82b17de
Louis Gobbe <gobbelouis@gmail.com>
2021-12-27 08:04:52 +0100

    404 imported + options page

--- 404.php
+++ 404.php
@@ -4,7 +4,30 @@ get_header();
 
 
 
-Page 404
+<section class="errorpage-section">
+<div class="errorpage-section-wrapper">
+    <p class="errorpage-subtitle gsap-title-reveal"><? the_field('errorpage_subtitle', 'options')?></p>
+    
+    <h1 class="errorpage-title">
+    <?
+            $maintitle = get_field('errorpage_title', 'options'); 
+            $openmainttitle = str_replace('[color]','<span>', $maintitle); 
+            $closemainttitle = str_replace('[/color]','</span>', $openmainttitle); 
+            echo $closemainttitle; 
+            ?></h1>
+    <p class="errorpage-text"><? the_field('errorpage_text', 'options')?></p>
+    <?php 
+    $link = get_field('errorpage_button', 'options');
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+    ?>
+    <a class="button_primary" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+        <?php echo $link_title; ?>
+
+    </a>
+</div>
+</section>
 
 
 <?php get_footer(); ?>
--- src/includes/options-page.php
+++ src/includes/options-page.php
@@ -1,5 +1,19 @@
 <?php
-///////// OPTIONS PAGE
-if (function_exists('acf_add_options_page')){
-    acf_add_options_page();
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'General Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		// 'capability'	=> 'edit_posts',
+        'icon_url' => 'dashicons-admin-generic',
+		'redirect'		=> true
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> '404 page',
+		'menu_title'	=> '404',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+		
 }
